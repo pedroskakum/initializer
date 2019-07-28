4c1bdb98865fc1bec5edbe4f076ed0765c6f5bfd
Pedro Paulo Skakum <pedroskakum@gmail.com>
2019-07-27 22:05:42 -0300

    Create install.sh
    Installing the most important packages for me.

---
+++ install.sh
@@ -0,0 +1,25 @@
+# Visual estudio code
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get install apt-transport-https
+sudo apt-get update --yes
+sudo apt-get install code --yes # or code-insiders
+
+# Google Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update --yes
+sudo apt install google-chrome-stable --yes
+
+# Virtual Box
+wget -q -O - http://download.virtualbox.org/virtualbox/debian/oracle_vbox_2016.asc | sudo apt-key add -
+sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian disco non-free contrib" >> /etc/apt/sources.list.d/virtualbox.org.list' 
+sudo apt update --yes
+sudo apt install virtualbox-6.0 --yes
+
+# Git
+sudo apt install git --yes
+
+# Curl
+sudo apt install curl --yes
