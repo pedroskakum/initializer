4f9ead4a838cabda45c500b18b11c420c8b4e8e8
Pedro Paulo Skakum <pedroskakum@gmail.com>
2019-07-29 17:22:39 -0300

    Add gitlab Runner

--- install.sh
+++ install.sh
@@ -88,4 +88,10 @@ sudo apt update
 sudo apt install docker-ce --yes
 sudo usermod -aG docker ${USER} # (for run docker without sudo)
 
+# Gitlab Runner
+sudo curl -L --output /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
+sudo chmod +x /usr/local/bin/gitlab-runner
+sudo useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
+sudo gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
+sudo gitlab-runner start
 
