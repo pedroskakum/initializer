c840461484a09e83ae164722ee4d18cc89b99a1b
Pedro Paulo Skakum <pedroskakum@gmail.com>
2019-08-07 22:07:52 -0300

    more efficient form to install apps

--- install.sh
+++ install.sh
@@ -11,76 +11,44 @@ echo "Type your name (for git): "
 read name
 echo "Type your email (for git): "
 read email
+echo "Type your system username (for docker, vscode extensions): "
+read username
 echo -n "If you have visual studio settings, replace in the vscode-settings.json file before we continue. Do you want to continue now? (y/n) "
 read answer
 
 if [ "$answer" != "${answer#[Yy]}" ] ;then
 
-# Visual estudio code
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-apt-get install --assume-yes --quiet apt-transport-https
-apt-get update
-apt-get install --assume-yes --quiet code # or code-insiders
-
-# Visual Studio Config
-cp --force vscode-settings.json > ~/.config/Code/User/
-
-# Google Chrome
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - 
-sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-apt-get update
-apt-get install --assume-yes --quiet google-chrome-stable
-
-# Virtual Box
-wget -q -O - http://download.virtualbox.org/virtualbox/debian/oracle_vbox_2016.asc | apt-key add -
-sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian disco non-free contrib" >> /etc/apt/sources.list.d/virtualbox.org.list' 
-apt-get update
-apt-get install --assume-yes --quiet virtualbox-6.0
-
-# Git
-apt-get install --assume-yes --quiet git
-git config --global user.email $email
-git config --global user.name $name
-apt-get install --assume-yes --quiet git-flow
-
-# Curl
 apt-get install --assume-yes --quiet curl
 
-# Node Version Manager
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg # VSCode
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - # Chrome
+wget -q -O - http://download.virtualbox.org/virtualbox/debian/oracle_vbox_2016.asc | apt-key add - # VirtualBox
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - # Kubectl
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - # Docker
 
-# Kubectl
-apt-get update
-apt-get install --assume-yes --quiet apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
-apt-get update
-apt-get install --assume-yes --quiet kubectl
+install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 
-# Minikube
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-chmod +x minikube
-install minikube /usr/local/bin
-rm minikube
-
-# Skaffold
-curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64
-chmod +x skaffold
-mv skaffold /usr/local/bin
-
-# Docker
-apt-get install --assume-yes --quiet ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list' # VSCode
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' # Chrome
+sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian disco non-free contrib" >> /etc/apt/sources.list.d/virtualbox.org.list' # VirtualBox
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list # Kubectl
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+
+# Update
 apt-get update
-apt-get install --assume-yes --quiet docker-ce
-usermod -aG docker ${USER} # (for run docker without sudo)
+
+# Install
+apt-get install --assume-yes --quiet apt-transport-https code google-chrome-stable virtualbox-6.0 git git-flow kubectl ca-certificates software-properties-common docker-ce snapd gimp
+
+# Pos Install
+cp --force vscode-settings.json > ~/.config/Code/User/ # Visual Studio Config
+git config --global user.email $email # Set git user
+git config --global user.name $name# # Set git email
+usermod -aG docker $username # (for run docker without sudo)
+
 
 # Docker Machine
-base=https://github.com/docker/machine/releases/download/v0.16.0 &&
-curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
+curl -L https://github.com/docker/machine/releases/download/v0.16.0/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
 install /tmp/docker-machine /usr/local/bin/docker-machine
 
 # Slack
@@ -133,6 +101,9 @@ snap install helm --classic
 # gcloud
 # echo "Not implemented yet"
 
+# Steam
+# echo "Not implemented yet"
+
 ssh-keygen -t ecdsa -b 521
 cat  ~/.ssh/id_ecdsa.pub
 
@@ -149,17 +120,32 @@ cat  ~/.ssh/id_ecdsa.pub
 # echo "ALL" | bash -c  "$(wget -qO- https://git.io/vQgMr)" 
 
 
-# Install VSCode Extensions
-# -i -u $USER bash << EOF
-# code --install-extension googlecloudtools.cloudcode
-# code --install-extension ms-azuretools.vscode-docker
-# code --install-extension fatihacet.gitlab-workflow
-# code --install-extension orta.vscode-jest
-# code --install-extension esbenp.prettier-vscode
-# code --install-extension ms-vscode.vscode-typescript-tslint-plugin
-# code --install-extension octref.vetur
-# code --install-extension vscode-icons-team.vscode-icons
-# EOF
+#Install VSCode Extensions
+-i -u $username bash << EOF
+code --install-extension googlecloudtools.cloudcode
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension fatihacet.gitlab-workflow
+code --install-extension orta.vscode-jest
+code --install-extension esbenp.prettier-vscode
+code --install-extension ms-vscode.vscode-typescript-tslint-plugin
+code --install-extension octref.vetur
+code --install-extension vscode-icons-team.vscode-icons
+EOF
+
+
+# Node Version Manager
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+
+# Minikube
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+chmod +x minikube
+install minikube /usr/local/bin
+rm minikube
+
+# Skaffold
+curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64
+chmod +x skaffold
+mv skaffold /usr/local/bin
 
 else
   echo "bye!"
